295861a5c0b9c0afe6e33d3f589153db800fae5c
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-04 14:52:25 -0500

    changes

--- dsl/first_dsl.groovy
+++ dsl/first_dsl.groovy
@@ -1,16 +1,18 @@
-pipelineJob('training/dsl/generated_job_from_dsl'){
-  definition {
-    cpsScm{
-      scm{
-        git{
-          remote{
-            url ('https://github.com/Jorge-Hoyos/jenkins-training.git')
-            credentials ('persona-github-hey')
+100.time {
+  pipelineJob('training/dsl/generated_job_from_dsl') {
+    definition {
+      cpsScm {
+        scm {
+          git {
+            remote {
+              url ('https://github.com/Jorge-Hoyos/jenkins-training.git')
+              credentials ('persona-github-hey')
+            }
+            branch('*/master')
           }
-          branch('*/master')
+          lightweight()
+          scriptPath('simple-pipes/pipe-as-code/Jenkinsfile')
         }
-        lightweight()
-        scriptPath('simple-pipes/pipe-as-code/Jenkinsfile')
       }
     }
   }
