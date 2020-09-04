5bc509f3839d0065d86ce6ce504416d648791dac
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-04 10:59:07 -0500

    changes

--- dsl/simple_dsl.groovy
+++ dsl/simple_dsl.groovy
@@ -1,4 +1,7 @@
 job('training/dsl/first_created_dsl_job_from_scm'){
+  triggers{
+    upstream ('training/dsl/dsl_pipeline_git_code', 'SUCCESS')
+  }
   steps {
     shell('echo hello world!')
     shell('echo hello world! number 2')
