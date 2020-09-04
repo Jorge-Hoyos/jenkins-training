f4c19f1f606d35781c13561339018d67ee91ee60
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-04 10:52:34 -0500

    reverting changes

--- dsl/simple_dsl.groovy
+++ dsl/simple_dsl.groovy
@@ -1,9 +1,6 @@
 job('training/dsl/first_created_dsl_job_from_scm'){
-  stages{
-    stage('first'){
-      steps {
-        shell('echo hello world!')
-      }
-    }
+  steps {
+    shell('echo hello world!')
+    shell('echo hello world! number 2')
   }
 }
