7c87298dd0004124a12f1683bf6227e8cbcbccf2
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-08 09:27:08 -0500

    changes

--- dsl/own/simple_folder.groovy
+++ dsl/own/simple_folder.groovy
@@ -1,14 +1,19 @@
 String myFolder = 'projects'
 String folderA = 'project-a'
+String folderB = 'project-b'
 
 folder (myFolder) {
   displayName (myFolder)
   description ('folder for all projects')
 }
 
-folder ('projects/project-a') {
+folder ("${myFolder}/${folderA}") {
   displayName (folderA)
   description ('folder for project a')
+}
+folder ("${myFolder}/${folderB}") {
+  displayName (folderB)
+  description ('folder for project b')
 }
 
 // folder ('other')
