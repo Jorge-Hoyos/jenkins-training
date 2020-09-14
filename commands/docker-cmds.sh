dcd6899e55069d6ce870bd5038ee9d96a0a9884a
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-14 09:16:49 -0500

    adding tf infrastructure

--- commands/docker-cmds.sh
+++ commands/docker-cmds.sh
@@ -1,6 +1,6 @@
 docker pull jenkins/jenkins:lts
 docker build -t jorge:latest .
-docker run -p 8080:8080 -d --name jorge -u root --mount source=jenkins_data,target=/var/jenkins_home jorge:latest
+docker run -p 8080:8080 -d --name jorge -u root -e JENKINS_VERSION:-2.249.1 --mount source=jenkins_data,target=/var/jenkins_home jorge:latest
 docker run -p 8080:8080 -d --name jenkins -u root --mount source=jenkins_data,target=/var/jenkins_home jenkins/jenkins:lts
 docker volume list
 docker exec jenkins_server cat /var/jenkins_home/secrets/initialAdminPassword
--- dsl/own/terraform-infra/role.tf
+++ dsl/own/terraform-infra/role.tf
@@ -16,5 +16,14 @@ resource "aws_iam_role" "jenkins_role" {
   assume_role_policy = data.aws_iam_policy_document.jenkins_assume_role_policy.json
   name               = "jenkins-role"
   path               = "/jenkins/"
-  tags               = var.default_tags
+}
+
+data "aws_iam_policy_document" "jenkins_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
