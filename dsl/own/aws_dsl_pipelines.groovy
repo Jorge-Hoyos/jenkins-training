374c6c344a306c3799e8e0e28e28a1f642e56bec
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-15 08:38:43 -0500

    chaning name variable

--- dsl/own/aws_dsl_pipelines.groovy
+++ dsl/own/aws_dsl_pipelines.groovy
@@ -1,7 +1,7 @@
 String scriptPath = 'dsl/own/scripts/aws-script.sh'
 String pipelinePath = 'training/dsl/my-pipelines/generated/aws-pipeline'
 
-job ("${folderPath}") {
+job ("${pipelinePath}") {
   steps {
     shell (readFileFromWorkspace(scriptPath))
   }
