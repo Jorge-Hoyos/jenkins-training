fdb3800209c090331d4ed9cc9805d2f88347bf98
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-15 08:22:55 -0500

    adding path to the pipeline

--- dsl/own/aws_dsl_pipelines.groovy
+++ dsl/own/aws_dsl_pipelines.groovy
@@ -1,7 +1,7 @@
 String scriptPath = 'dsl/own/scripts/aws-script.sh'
 pipelinePath = 'training/dsl/my-pipelines/generated'
 
-job ('aws-pipeline') {
+job ($pipelinePath) {
   steps {
     shell (readFileFromWorkspace(scriptPath))
   }
