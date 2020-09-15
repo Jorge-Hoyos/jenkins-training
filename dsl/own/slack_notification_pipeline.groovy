e415bf828aa271d6f2d4e7a5a6dcde10f205fe86
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-15 14:40:29 -0500

    fixing slack notifier

--- dsl/own/slack_notification_pipeline.groovy
+++ dsl/own/slack_notification_pipeline.groovy
@@ -4,23 +4,22 @@ job ("${pipelinePath}") {
   steps {
     shell ('echo algo')
   }
-  publishers {
-    jenkins.plugins.slack.SlackNotifier (plugin = 'slack@2.42') {
-      startNotification(true)
-      notifySuccess(true)
-      notifyAborted(true)
-      notifyNotBuilt(true)
-      notifyUnstable(true)
-      notifyRegression(true)
-      notifyFailure(false)
-      notifyEveryFailure(true)
-      notifyBackToNormal(true)
-      notifyRepeatedFailure(false)
-      includeTestSummary(false)
-      includeFailedTests(false)
-      uploadFiles(false)
-      commitInfoChoice('NONE')
-      includeCustomMessage(false)
-    }
+  configure { project ->
+    project / publishers << 'jenkins.plugins.slack.SlackNotifier'
+    startNotification(true)
+    notifySuccess(true)
+    notifyAborted(true)
+    notifyNotBuilt(true)
+    notifyUnstable(true)
+    notifyRegression(true)
+    notifyFailure(false)
+    notifyEveryFailure(true)
+    notifyBackToNormal(true)
+    notifyRepeatedFailure(false)
+    includeTestSummary(false)
+    includeFailedTests(false)
+    uploadFiles(false)
+    commitInfoChoice('NONE')
+    includeCustomMessage(false)
   }
 }
