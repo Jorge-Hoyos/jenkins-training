a4eebe4764d86ef656d6a9e395520acc778e367d
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-15 08:33:58 -0500

    adding description to folder

--- dsl/own/aws_dsl_pipelines.groovy
+++ dsl/own/aws_dsl_pipelines.groovy
@@ -1,7 +1,9 @@
 String scriptPath = 'dsl/own/scripts/aws-script.sh'
 String folderPath = 'training/dsl/my-pipelines/generated'
 
-folder ('training/dsl/my-pipelines/generated')
+folder (folderPath) {
+  description ('folder for dsl generated pipelines')
+}
 
 job ("${folderPath}/aws-pipeline") {
   steps {
