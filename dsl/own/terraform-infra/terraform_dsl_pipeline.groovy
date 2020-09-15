d970e2bb1b7c6472f627051c2b53c46d7e6d5e8f
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-15 09:00:52 -0500

    moving terraform pipeline to generated fodler

--- dsl/own/terraform-infra/terraform_dsl_pipeline.groovy
+++ dsl/own/terraform-infra/terraform_dsl_pipeline.groovy
@@ -1,4 +1,6 @@
-job ('terraform-pipeline') {
+String pipelinePath = 'training/dsl/my-pipelines/generated/terraform_pipeline'
+
+job ("${pipelinePath}") {
   steps {
     shell ('terraform init')
   }
