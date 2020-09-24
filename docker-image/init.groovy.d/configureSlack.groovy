f62aa016bd7790bd7602263b0517b5c3dd0efe9a
Jorge Hoyos <jorge.hoyos@globant.com>
2020-09-24 09:17:12 -0500

    adding things to configure slack

--- docker-image/init.groovy.d/configureSlack.groovy
+++ docker-image/init.groovy.d/configureSlack.groovy
@@ -12,16 +12,20 @@ slackSecretDescription = env['SLACK_DESCRIPTION']
 mySecret = Secret.fromString (slackSecretValue)
 secretId = 'slackSecret'
 slackDomain = 'globant'
+slackRoom = 'training-slack-jenkins-jorge'
 
 secretText = new StringCredentialsImpl(CredentialsScope.GLOBAL, secretId, slackSecretDescription, mySecret)
 SystemCredentialsProvider.instance.store.addCredentials(Domain.global(), secretText)
 
-slackBuilder = SlackNotifier.builder()
-slackNotifier = slackBuilder.build()
+// slackBuilder = SlackNotifier.builder()
+// slackNotifier = slackBuilder.build()
+// slackDescriptor = slackNotifier.descriptor
 
-slackDescriptor = slackNotifier.descriptor
+slackDescriptor = new DescriptorImpl()
 slackDescriptor.teamDomain = slackDomain
 slackDescriptor.tokenCredentialId = secretId
+slackDescriptor.room = slackRoom
+slackDescriptor.doTestConnectionGlobal(slackDescriptor.baseUrl, slackDescriptor.teamDomain, slackDescriptor.tokenCredentialId, slackDescriptor.botUser, slackDescriptor.room, slackDescriptor.sendAsText, slackDescriptor.iconEmoji, slackDescriptor.username)
 println (slackDescriptor.teamDomain)
 
 slackDescriptor.save()
